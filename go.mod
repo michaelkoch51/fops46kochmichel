d6c2d7d0c2737f5fbf0eb6ae24d58881d07f3e32
Michael Kochnev <misha.kochnev@gmail.com>
2026-02-20 14:48:48 +0000

    Add Go files: main.go, main_test.go and go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module fops46kochmichel
+
+go 1.21.6
---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestHelloOutput(t *testing.T) {
+    // Простой тест — пока просто заглушка
+}
+
+func ExampleHello() {
+    main()
+    // Output:
+    // Hello from Go project in Jenkins!
+}
+EOF
+
